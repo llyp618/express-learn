c00732267625ca01c052d69d5f7cba45a24b827c
llyp618 <llyp618@163.com>
2017-01-23 17:20:39 +0800

    111

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-module.exports = function(app){   //返回一个函数 被index.js 调用下就可以了，以免路由过多导致程序主入口 index.js臃肿
-	app.get('/',function(req,res){
-		res.redirect('/posts');
-	});
-	app.use('/signup',require('./signup'));
-	app.use('/signin',require('./signin'));
-	app.use('/signout',require('./signout'));
-	app.use('/posts',require('./posts'));
+module.exports = function (app) {
+  app.get('/', function (req, res) {
+    res.redirect('/posts');
+  });
+  app.use('/signup', require('./signup'));
+  app.use('/signin', require('./signin'));
+  app.use('/signout', require('./signout'));
+  app.use('/posts', require('./posts'));
 };
--- routes/signout.js
+++ routes/signout.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var router = express.Router();
 
@@ -5,7 +6,10 @@ var checkLogin = require('../middlewares/check').checkLogin;
 
 // GET /signout 登出
 router.get('/', checkLogin, function(req, res, next) {
-  res.send(req.flash());
+	// 清空 session 中的用户信息
+	req.session.user = null;
+	req.flash('success','登出成功');
+  res.redirect('/posts');
 });
 
 module.exports = router;
